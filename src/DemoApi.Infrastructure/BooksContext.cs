838265a35e04b899aac6475619208f3c864ec9b8
ahorbunov-epam <Artem_Horbunov1@epam.com>
2024-07-20 16:50:53 +0300

    Seed a default user as example

--- src/DemoApi.Infrastructure/BooksContext.cs
+++ src/DemoApi.Infrastructure/BooksContext.cs
@@ -12,4 +12,26 @@ public class BooksContext(DbContextOptions<BooksContext> options) : IdentityDbCo
     public DbSet<Form> Forms { get; set; }
 
     public DbSet<Reader> Readers { get; set; }
+    
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        // Seed a default user
+        var hasher = new PasswordHasher<IdentityUser>();
+        var passwordHash = hasher.HashPassword(null, "P@f0iujKTI9pWIPqRsem~");
+
+        modelBuilder.Entity<IdentityUser>().HasData(
+            new IdentityUser
+            {
+                Id = "default-user-id",
+                UserName = "default@example.com",
+                NormalizedUserName = "DEFAULT@EXAMPLE.COM", 
+                Email = "default@example.com",
+                NormalizedEmail = "DEFAULT@EXAMPLE.COM",
+                PasswordHash = passwordHash,
+                EmailConfirmed = false,
+                LockoutEnabled = true
+            });
+    }
 }
