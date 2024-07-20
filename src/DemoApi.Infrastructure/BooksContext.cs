efef3d3910e1411e0ee7a2ceecade112642ad13d
ahorbunov-epam <Artem_Horbunov1@epam.com>
2024-07-20 15:58:04 +0300

    Add default identity

--- src/DemoApi.Infrastructure/BooksContext.cs
+++ src/DemoApi.Infrastructure/BooksContext.cs
@@ -1,9 +1,11 @@
 using DemoApi.Core.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace DemoApi.Infrastructure;
 
-public class BooksContext(DbContextOptions<BooksContext> options) : DbContext(options)
+public class BooksContext(DbContextOptions<BooksContext> options) : IdentityDbContext<IdentityUser>(options)
 {
     public DbSet<Book> Books { get; set; }
 
